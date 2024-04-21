a645e1fc543896d19959faf59ebd61e688ce81a3
abmusawer-f <elearningedtech02@gmail.com>
2024-04-21 06:47:11 +0430

    working on profile

--- resources/views/pages/profile/index.blade_backup.php
+++ resources/views/pages/profile/index.blade_backup.php
@@ -8,7 +8,7 @@ body{
     margin-top:20px;
     color: #1a202c;
     text-align: left;
-    background-color: #e2e8f0;
+    background-color: #e2e8f0;    
 }
 .main-body {
     padding: 15px;
@@ -181,7 +181,7 @@ body{
 
 <div class="container">
     <div class="main-body">
-
+    
           <!-- Breadcrumb -->
           <nav aria-label="breadcrumb" class="main-breadcrumb">
             <ol class="breadcrumb">
@@ -191,42 +191,58 @@ body{
             </ol>
           </nav>
           <!-- /Breadcrumb -->
-<div class="row" style="background: white">
-    <div class="col-md-4">
-        <img src="{{asset($rec['profile_image'])}}" alt="Admin" class="rounded-circle" width="150">
-    </div>
-    <div class="col-md-8">
-        <div class="row">
-            <div class="col-sm-3">
-              <h2 class="mb-0">Profile Information</h2>
-            </div>
-
-          </div>
-          <div class="row">
-            <div class="col-sm-3">
-              <h6 class="mb-0">Current Grade</h6>
-            </div>
-            <div class="col-sm-9 text-secondary">
-              {{$rec['grade_name']}}
-            </div>
-          </div>
-    </div>
-</div>
+    
           <div class="row gutters-sm">
-
-            <div class="row">
-                <div class="col-md-4 mb-3">
-
-
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="{{asset($rec['profile_image'])}}" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4>{{$rec['student_name']}} </h4>
+                      <p class="text-secondary mb-1">{{$rec['student_gender']}}</p>
+                      <p class="text-muted font-size-sm">{{$rec['school_name']}}</p>
+                      <p class="text-muted font-size-sm">{{$rec['province_name']}} - {{$rec['district_name']}}</p>
+                      {{-- <button class="btn btn-primary">Follow</button>
+                      <button class="btn btn-outline-primary">Message</button> --}}
+                    </div>
+                  </div>
                 </div>
-              <div class="card col-md-8 mb-3">
+              </div>
+              {{-- <div class="card mt-3">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
+                    <span class="text-secondary">https://bootdey.com</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
+                    <span class="text-secondary">bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
+                    <span class="text-secondary">@bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
+                    <span class="text-secondary">bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
+                    <span class="text-secondary">bootdey</span>
+                  </li>
+                </ul>
+              </div> --}}
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
                 <div class="card-body">
                   <div class="row">
                     <div class="col-sm-3">
                       <h6 class="mb-0">Current Grade</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                      {{$rec['grade_name']}}
+                      {{$rec['grade_name']}} 
                     </div>
                   </div>
                   <hr>
@@ -235,7 +251,7 @@ body{
                       <h6 class="mb-0">Email</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                        {{$rec['student_email']}}
+                        {{$rec['student_email']}} 
                     </div>
                   </div>
                   <hr>
@@ -295,12 +311,12 @@ body{
                                   <a data-toggle="collapse" href="#collapse{{$i.$b}}">{{$rec['grades'][$i]['subjects'][$b]['subject_name']}}</a>
                                 </h4>
                               </div>
-
+                              
                               <div id="collapse{{$i.$b}}" class="panel-collapse collapse">
                                 {!! (count($rec['grades'][$i]['subjects'][$b]['chapters']) !=0)?"<h3>chapters</h3>":"" !!}
                                 @for ($c = 0; $c < count($rec['grades'][$i]['subjects'][$b]['chapters']) ; $c++)
                                 <ul class="list-group">
-
+                                  
                                     <li class="list-group-item">
                                       <table>
                                         <thead>
@@ -317,7 +333,7 @@ body{
                                         </tbody>
                                       </table>
                                     </li>
-
+                                  
                                 </ul>
                                 @endfor
                                 {{-- <div class="panel-footer">Footer</div> --}}
@@ -325,42 +341,42 @@ body{
                             </div>
                           </div>
                         @endfor
-
+                        
                     </div>
                     @endfor
                     <!-- active tab on page load gets checked attribute -->
                     {{-- <input type="radio" id="tab1" name="tabGroup1" class="tab" checked>
                     <label for="tab1">Short</label>
-
+                
                     <input type="radio" id="tab2" name="tabGroup1" class="tab">
                     <label for="tab2">Medium</label>
-
+                
                     <input type="radio" id="tab3" name="tabGroup1" class="tab">
                     <label for="tab3">Long</label>
 
                     <input type="radio" id="tab4" name="tabGroup1" class="tab">
                     <label for="tab4">test</label>
-
+                
                     <div class="tab__content">
                       <h3>Short Section</h3>
                       <p>Praesent nonummy mi in odio. Nullam accumsan lorem in dui. Vestibulum turpis sem, aliquet eget, lobortis pellentesque, rutrum eu, nisl. Nullam accumsan lorem in dui. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
                     </div>
-
+                
                     <div class="tab__content">
                       <h3>Medium Section</h3>
                       <p>Praesent nonummy mi in odio. Nullam accumsan lorem in dui. Vestibulum turpis sem, aliquet eget, lobortis pellentesque, rutrum eu, nisl. Nullam accumsan lorem in dui. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-
+                
                       <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Morbi mattis ullamcorper velit. Pellentesque posuere. Etiam ut purus mattis mauris sodales aliquam. Praesent nec nisl a purus blandit viverra.</p>
                     </div>
-
+                
                     <div class="tab__content">
                       <h3>Long Section</h3>
                       <p>Praesent nonummy mi in odio. Nullam accumsan lorem in dui. Vestibulum turpis sem, aliquet eget, lobortis pellentesque, rutrum eu, nisl. Nullam accumsan lorem in dui. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-
+                
                       <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Morbi mattis ullamcorper velit. Pellentesque posuere. Etiam ut purus mattis mauris sodales aliquam. Praesent nec nisl a purus blandit viverra.</p>
-
+                
                       <p>Praesent nonummy mi in odio. Nullam accumsan lorem in dui. Vestibulum turpis sem, aliquet eget, lobortis pellentesque, rutrum eu, nisl. Nullam accumsan lorem in dui. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-
+                
                       <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Morbi mattis ullamcorper velit. Pellentesque posuere. Etiam ut purus mattis mauris sodales aliquam. Praesent nec nisl a purus blandit viverra.</p>
                     </div>
 
@@ -368,7 +384,7 @@ body{
                       <h3>test Section</h3>
                       <p>test</p>
                     </div> --}}
-
+                
                   </div>
                   {{-- <div class="card h-100">
                     <div class="card-body">
