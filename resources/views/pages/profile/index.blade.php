a9f0758a2f7f9fc9ae87ebc427788b377dc732f4
edtecheqra <165984611+edtecheqra@users.noreply.github.com>
2024-04-21 10:04:21 +0000

    profile page links fixed

--- resources/views/pages/profile/index.blade.php
+++ resources/views/pages/profile/index.blade.php
@@ -209,8 +209,8 @@
     <!-- Breadcrumb -->
     <nav aria-label="breadcrumb" class="main-breadcrumb">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
+        <li class="breadcrumb-item"><a href="/">Home</a></li>
+        <li class="breadcrumb-item"><a href="/report/index">Users</a></li>
         <li class="breadcrumb-item active" aria-current="page">User Profile</li>
       </ol>
     </nav>
