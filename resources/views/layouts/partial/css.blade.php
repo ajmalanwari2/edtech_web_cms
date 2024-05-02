df868bb844c9e7de44d8df8eeaaff13b4ecc0e80
ajmal <aanwari02@gmail.com>
2024-05-02 09:36:59 +0430

    bootstrap is added

--- resources/views/layouts/partial/css.blade.php
+++ resources/views/layouts/partial/css.blade.php
@@ -36,7 +36,7 @@
 <link rel="stylesheet" href="{{ asset('assets/datatables/buttons.dataTables.min.css') }}" />
 @stack('custom-css')
 <link href="{{ asset('assets/landing/css/icons.css') }}" rel="stylesheet" />    
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link href="{{ asset('assets/landing/css/bootstrap.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/landing/css/style.css') }}" rel="stylesheet" />
 
 
